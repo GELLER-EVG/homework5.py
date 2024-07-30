7f5059f075a399752a533727485b37aa52538eb6
evgenia kolmakova <evgeniageller000@gmail.com>
2024-07-30 15:39:40 +0300

    Update and rename homework5.py to module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -5,7 +5,7 @@ print(immutable_va)
 # print(immutable_va+stempl)
 # "изменить кортеж не получится для более недёжной защиты элиментов данных-баз, особенность кортежа- занамает меньше места "
 
-mutable_list = (100, [99, 88], "Stop") + (101, 102)
+mutable_list = [100, [99, 88], "Stop", 101, 102]
 print(mutable_list)
 mutable_list [1][1]= 99
 print(mutable_list)
